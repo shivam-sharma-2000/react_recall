9ecf1b27c5e1255b604717a4c355479587dcf566
androidOsswal <android@osswalinfo.com>
2024-09-10 18:00:37 +0530

    complited hover effect add to list effect

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,5 +1,8 @@
 // src/components/Card.js
+import { useState } from "react";
 import "../Card.css"; // Import the CSS file for styling
+import { IoIosAdd  } from "react-icons/io";
+import {IoIosCheckmark } from "react-icons/io";
 
 interface Props {
   title: String;
@@ -7,10 +10,42 @@ interface Props {
   image: string;
 }
 const Card = ({ title, content, image }: Props) => {
-  return (
-    <div className="movie-card">
-      <img src={image} alt="new" className="card-image" />
-    </div>
+  var [isHover, setHover] = useState(false);
+  var [isLike, setLike] = useState(false);
+  return (<div
+    className="movie-card"
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => {
+      if (!isLike) setHover(false)
+    }}
+  // style={{ background: `url(${image})` }}
+  >
+    {(
+      <div className="card-container">
+        <img src={image} alt="new" className="card-image" />
+        {
+          isLike
+            ? <IoIosCheckmark  
+              style={{ color: 'red' }} className="card-like-button"
+              onClick={() => {
+                setLike(false);
+              }}
+            />
+            :
+            isHover
+              ?
+              <IoIosAdd 
+                style={{ color: 'white' }} className="card-like-button"
+                onClick={() => {
+                  setLike(true);
+                }}
+              />
+              : ""
+
+        }
+      </div>
+    )}
+  </div>
   );
 };
 
